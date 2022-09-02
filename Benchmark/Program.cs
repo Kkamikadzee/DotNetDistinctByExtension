0b2f23ecb4c8fdaff098b0ca270c6f067f9b8b5b
Kkamikadzee <Kkamikadzee@yandex.ru>
2022-09-02 17:55:36 +0300

    Initial commit

---
+++ Benchmark/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark;
+
+public class Benchmark
+{
+    private class FooBar
+    {
+        public string Foo { get; init; } = string.Empty;
+        public string Bar { get; init; } = string.Empty;
+        public DateTime Baz { get; init; }
+    }
+
+    private const string Chars = "123";
+    private const int CountCharsInString = 32;
+    private const int SampleSize = 30;
+    private const int NumberSamples = 1000;
+    private const int TimeDelta = 10;
+
+    private readonly List<List<FooBar>> _randomSets;
+
+    public Benchmark()
+    {
+        var random = new Random();
+        _randomSets = new List<List<FooBar>>(NumberSamples);
+
+        for (int sampleCounter = 0; sampleCounter < NumberSamples; ++sampleCounter)
+        {
+            _randomSets.Add(new List<FooBar>(SampleSize));
+            for (int objectCounter = 0; objectCounter < SampleSize; objectCounter++)
+            {
+                string fooChar = Chars[random.Next(Chars.Length)].ToString();
+                string barChar = Chars[random.Next(Chars.Length)].ToString();
+                var baz = DateTime.Now.AddHours(random.Next(TimeDelta));
+
+                _randomSets[sampleCounter].Add(new FooBar()
+                {
+                    Foo = new StringBuilder().Insert(0, fooChar, CountCharsInString).ToString(),
+                    Bar = new StringBuilder().Insert(0, barChar, CountCharsInString).ToString(),
+                    Baz = baz
+                });
+            }
+        }
+    }
+
+    [Benchmark(Description = "Sort")]
+    public void BenchSort()
+    {
+        LinkedList<LinkedList<FooBar>> result = new LinkedList<LinkedList<FooBar>>();
+
+        foreach (var sampleSet in _randomSets)
+        {
+            result.AddLast(
+                new LinkedList<FooBar>(
+                    sampleSet.DistinctByWithMaxSorted(f => (f.Foo, f.Bar), f => f.Baz)));
+        }
+    }
+
+    [Benchmark(Description = "Hash")]
+    public void BenchHash()
+    {
+        LinkedList<LinkedList<FooBar>> result = new LinkedList<LinkedList<FooBar>>();
+
+        foreach (var sampleSet in _randomSets)
+        {
+            result.AddLast(
+                new LinkedList<FooBar>(
+                    sampleSet.DistinctByWithMax(f => (f.Foo, f.Bar), f => f.Baz)));
+        }
+    }
+}
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<Benchmark>();
+    }
+}
---
+++ DotNetDistinctByExtension/DistinctByExtension.cs
@@ -0,0 +1,97 @@
+﻿namespace System.Linq;
+
+public static class DistinctByExtension
+{
+    /// <summary>
+    /// Returns sorted distinct elements from a sequence according to a specified key selector function and using a specified comparer to compare keys, with the remaining elements having the maximum keySort value. 
+    /// </summary>
+    /// <param name="source">The sequence to remove duplicate elements from.</param>
+    /// <param name="keySelector">A function to extract the key to distinguish for each element.</param>
+    /// <param name="keySort">A function to extract the key maximizing for each element.</param>
+    /// <param name="keySelectorComparer">An IComparer to compare distinguish keys.</param>
+    /// <param name="keySortComparer">An IComparer to compare maximizing keys.</param>
+    /// <param name="duplicateComparer">An IEqualityComparer to compare maximizing keys.</param>
+    /// <typeparam name="TSource">The type of elements of source.</typeparam>
+    /// <typeparam name="TKey">The type of key to distinguish elements by.</typeparam>
+    /// <typeparam name="TSort">The type of key to maximizing elements by.</typeparam>
+    /// <returns>An IEnumerable that contains distinct elements from the source sequence, with the elements having the maximum TSort value.</returns>
+    /// <exception cref="ArgumentException">source is null.</exception>
+    /// <remarks>This method is implemented by using deferred execution.
+    /// The immediate return value is an object that stores all the information that is required to perform the action.
+    /// The query represented by this method is not executed until the object is enumerated either by calling its GetEnumerator method directly or by using foreach in Visual C#.</remarks>
+    public static IEnumerable<TSource> DistinctByWithMaxSorted<TSource, TKey, TSort>(this IEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector, Func<TSource, TSort> keySort,
+        IComparer<TKey>? keySelectorComparer = null, IComparer<TSort>? keySortComparer = null,
+        IEqualityComparer<TKey>? duplicateComparer = null)
+    {
+        if (source is null)
+            throw new ArgumentException("source is null.");
+
+        duplicateComparer ??= EqualityComparer<TKey>.Default;
+
+        var sortedCollection = source.OrderByDescending(keySelector, keySelectorComparer)
+            .ThenByDescending(keySort, keySortComparer);
+
+        using var sortedCollectionEnumerator = sortedCollection.GetEnumerator();
+
+        if (!sortedCollectionEnumerator.MoveNext())
+            yield break;
+
+        TSource previousObject = sortedCollectionEnumerator.Current;
+        yield return previousObject;
+
+        while (sortedCollectionEnumerator.MoveNext())
+        {
+            TSource currentObject = sortedCollectionEnumerator.Current;
+
+            if (duplicateComparer.Equals(keySelector(previousObject), keySelector(currentObject))) continue;
+
+            yield return currentObject;
+            previousObject = currentObject;
+        }
+    }
+
+    /// <summary>
+    /// Returns sorted distinct elements from a sequence according to a specified key selector function and using a specified comparer to compare keys, with the remaining elements having the maximum keySort value. 
+    /// </summary>
+    /// <param name="source">The sequence to remove duplicate elements from.</param>
+    /// <param name="keySelector">A function to extract the key to distinguish for each element.</param>
+    /// <param name="keySort">A function to extract the key maximizing for each element.</param>
+    /// <param name="keySortComparer">An IComparer to compare maximizing keys.</param>
+    /// <param name="duplicateComparer">An IEqualityComparer to compare maximizing keys.</param>
+    /// <typeparam name="TSource">The type of elements of source.</typeparam>
+    /// <typeparam name="TKey">The type of key to distinguish elements by.</typeparam>
+    /// <typeparam name="TSort">The type of key to maximizing elements by.</typeparam>
+    /// <returns>An IEnumerable that contains distinct elements from the source sequence, with the elements having the maximum TSort value.</returns>
+    /// <exception cref="ArgumentException">source is null.</exception>
+    public static IEnumerable<TSource> DistinctByWithMax<TSource, TKey, TSort>(this IEnumerable<TSource> source,
+        Func<TSource, TKey> keySelector, Func<TSource, TSort> keySort,
+        IComparer<TSort>? keySortComparer = null, IEqualityComparer<TKey>? duplicateComparer = null)
+        where TKey : notnull
+    {
+        if (source is null)
+            throw new ArgumentException("source is null.");
+
+        keySortComparer ??= Comparer<TSort>.Default;
+
+        var distinctDictionary = new Dictionary<TKey, TSource>(duplicateComparer);
+
+        foreach (var objectInCollection in source)
+        {
+            if (distinctDictionary.TryGetValue(keySelector(objectInCollection), out TSource? objectInSet))
+            {
+                if (keySortComparer.Compare(keySort(objectInSet), keySort(objectInCollection)) < 0)
+                {
+                    distinctDictionary.Remove(keySelector(objectInSet));
+                    distinctDictionary.Add(keySelector(objectInCollection), objectInCollection);
+                }
+
+                continue;
+            }
+
+            distinctDictionary.Add(keySelector(objectInCollection), objectInCollection);
+        }
+
+        return distinctDictionary.Values;
+    }
+}
